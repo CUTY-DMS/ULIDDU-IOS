9c1d1619a6957f3302a2225d3e50b88660c39f0b
junha <goodjunha@gmail.com>
2022-07-29 20:15:14 +0900

    ♻️:: Delete unnecessary files

--- OUREDO/OUREDO/calendarVC.swift
+++
@@ -1,8 +0,0 @@
-//
-//  calendarVC.swift
-//  OUREDO
-//
-//  Created by 박준하 on 2022/07/05.
-//
-
-import
--- OUREDO/OUREDO/toDoList/signUpText.swift
+++
@@ -1,8 +0,0 @@
-//
-//  signUpText.swift
-//  OUREDO
-//
-//  Created by 박준하 on 2022/07/13.
-//
-
-import Foundation
