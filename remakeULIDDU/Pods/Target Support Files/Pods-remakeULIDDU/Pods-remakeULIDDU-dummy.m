c9bbc7e6dc6aa4a0bc3d252c62f20bb71904a065
junha <goodjunha@gmail.com>
2022-08-12 10:40:36 +0900

    .

---
+++ remakeULIDDU/Pods/Target Support Files/Pods-remakeULIDDU/Pods-remakeULIDDU-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_remakeULIDDU : NSObject
+@end
+@implementation PodsDummy_Pods_remakeULIDDU
+@end
---
+++ remakeULIDDU/remakeULIDDU/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  mainViewController.swift
+//  remakeULIDDU
+//
+//  Created by 박준하 on 2022/08/12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = 
+    }
+
+}
